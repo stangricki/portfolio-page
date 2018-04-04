7446d2ea53b181b59229ecd16d6bb12da5b0e3a1
Filip Stangricki <stangricki@gmail.com>
2018-04-04 20:13:58 +0200

    Added fadeIn on home section

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
 const links = Array.from(document.querySelectorAll('.tabnav'));
-
+$('.tabcontent-active').fadeIn();
 links.forEach(link => link.addEventListener('click', function () {
 	const activeLink = document.querySelector('.tabnav-active')
 	activeLink.classList.remove('tabnav-active') // searches for class 'active' in activeLink Array and removes it
