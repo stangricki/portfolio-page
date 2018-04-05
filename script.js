0405e2eb23d3e9c30f8099821dc799bb8190ea1e
Filip Stangricki <stangricki@gmail.com>
2018-04-05 13:10:28 +0200

    Work section - added interactive

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-
-
-
 $(document).ready(function() {
 
-const links = Array.from(document.querySelectorAll('.tabnav'));
-$('.tabcontent-active').fadeIn();
-links.forEach(link => link.addEventListener('click', function () {
-	const activeLink = document.querySelector('.tabnav-active')
-	activeLink.classList.remove('tabnav-active') // searches for class 'active' in activeLink Array and removes it
-	if(!link.classList.contains("tabnav-active")) {
-		link.classList.add('tabnav-active');
-		$('.tabcontent').fadeOut(0);
+	const links = Array.from(document.querySelectorAll('.tabnav'));
+	$('.tabcontent-active').fadeIn();
+	links.forEach(link => link.addEventListener('click', function () {
+		const activeLink = document.querySelector('.tabnav-active')
+		activeLink.classList.remove('tabnav-active') // searches for class 'active' in activeLink Array and removes it
+		if(!link.classList.contains("tabnav-active")) {
+			link.classList.add('tabnav-active');
+			$('.tabcontent').fadeOut(0);
 
-	} // adds class 'active' to clicked
-	
-    const currentTab = document.querySelector('.tabcontent-active');
-    if (currentTab) currentTab.classList.remove('tabcontent-active');
-    document.getElementById(link.dataset.target).classList.add('tabcontent-active'); 
-		$('.tabcontent-active').fadeIn();
-}));
+		} // adds class 'active' to clicked
+		
+	    const currentTab = document.querySelector('.tabcontent-active');
+	    if (currentTab) currentTab.classList.remove('tabcontent-active');
+	    document.getElementById(link.dataset.target).classList.add('tabcontent-active'); 
+			$('.tabcontent-active').fadeIn();
+	}));
 
 
 
